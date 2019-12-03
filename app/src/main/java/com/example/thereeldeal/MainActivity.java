b2125935a28ccff043d2c386f7283c1fa4dfaffb
alyciab2 <alyciab2@illinois.edu>
2019-12-03 15:06:51 -0600

    trying response api stuff

--- app/src/main/java/com/example/thereeldeal/MainActivity.java
+++ app/src/main/java/com/example/thereeldeal/MainActivity.java
@@ -10,14 +10,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
 
         EditText searchBox = findViewById(R.id.editText);
         Button searchButton = findViewById(R.id.searchButton);
@@ -31,14 +41,25 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 populateSearchResults(searchBox.getText().toString().trim().toLowerCase());
                 setContentView(R.layout.search_results);
-                Button searchDifferent = findViewById(R.id.searchDifferent);
-                searchDifferent.setOnClickListener(notclicked -> {
-                    startActivity(new Intent(this, MainActivity.class));
-                });
+                TextView test = findViewById(R.id.test);
+                String url = "http://www.omdbapi.com/?i=tt0848228&apikey=f5c82c9f";
+                JsonObjectRequest request = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                test.setText("Response: " + response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                test.setText("Oh no");
+
+                            }
+                        });
+                queue.add(request);
+
             }
         });
-
-
     }
     //helper methods
     public void populateSearchResults(String movieName) {
@@ -49,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
                 underscoreName = individualWords[i] + "_";
             }
         }
+        //TextView test = findViewById(R.id.test);
+
 
     }
 }
