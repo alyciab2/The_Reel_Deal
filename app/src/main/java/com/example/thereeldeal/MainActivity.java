88e021d4c5c3f2181823e9b1d4469b1b031bab03
alyciab2 <alyciab2@illinois.edu>
2019-12-04 15:15:53 -0600

    images work but chunks dont

--- app/src/main/java/com/example/thereeldeal/MainActivity.java
+++ app/src/main/java/com/example/thereeldeal/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,8 +19,19 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONObject;
+import org.json.JSONArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
@@ -56,11 +69,12 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         setContentView(R.layout.search_results);
+        LinearLayout searchResults = findViewById(R.id.searchScrollLayout);
         Button searchDifferent = findViewById(R.id.searchDifferent);
         searchDifferent.setOnClickListener(notclicked -> {
             startActivity(new Intent(this, MainActivity.class));
         });
-        TextView test = findViewById(R.id.test);
+        //TextView test = findViewById(R.id.test);
         String urlFirst = "https://www.omdbapi.com/?s=";
         String urlSecond = "&apikey=f5c82c9f";
 
@@ -73,12 +87,46 @@ public class MainActivity extends AppCompatActivity {
                             TextView errorMessage = findViewById(R.id.errorMessage);
                             errorMessage.setVisibility(View.VISIBLE);
                         }
-                        test.setText("Response: " + response.toString());
+                        try {
+                            JSONArray movieArray = response.getJSONArray("Search");
+                            for (int i = 0; i < 1; i++) {
+                                setContentView(R.layout.chunk_search_result);
+                                View infoChunk = getLayoutInflater().inflate(R.layout.chunk_search_result, searchResults, false);
+                                TextView movieTitle = findViewById(R.id.movieSearchTitle);
+                                ImageView movieImage = findViewById(R.id.movieSearchImage);
+                                Button moreInfo = findViewById(R.id.moreInfo);
+                                movieTitle.setText(movieArray.getJSONObject(i).get("Title").toString());
+                                String imageURL = movieArray.getJSONObject(i).get("Poster").toString();
+                                Picasso.get().load(imageURL).into(movieImage);
+                                searchResults.addView(infoChunk);
+                                //setContentView(R.layout.search_results);
+                            }
+                        } catch (JSONException e) {
+                            System.out.println(e.toString());
+                        }
+                        //JsonElement JsonResult = new JsonParser().parse(response.toString());
+                        //test.setText(JsonResult.getAsJsonObject().get("Search").getAsString());
+                        /*
+                        JsonArray movieArray = JsonResult.get("Search").getAsJsonArray();
+                        for (JsonElement movieInfo : movieArray) {
+                            test.setVisibility(View.GONE);
+                            setContentView(R.layout.chunk_search_result);
+                            View infoChunk = getLayoutInflater().inflate(R.layout.chunk_search_result, searchResults, false);
+                            TextView movieTitle = findViewById(R.id.movieSearchTitle);
+                            ImageView movieImage = findViewById(R.id.movieSearchImage);
+                            Button moreInfo = findViewById(R.id.moreInfo);
+                            JsonObject movie = movieInfo.getAsJsonObject();
+                            test.setText(movie.get("Title").getAsString());
+                            searchResults.addView(infoChunk);
+                            setContentView(R.layout.search_results);
+                        } */
+
+
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        test.setText("Something went wrong. Please check your connection.");
+                        //test.setText("Something went wrong. Please check your connection.");
                     }
                 });
         queue.add(request);
